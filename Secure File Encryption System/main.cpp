6821d1d9d00778d4fcef31d188af565cf6a7b8fc
Muhammad Bilal Noshahi <mbilalnoshahi@hotmail.com>
2024-05-12 22:16:05 +0500

    Started the project

--- Secure File Encryption System/main.cpp
+++ Secure File Encryption System/main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+	// Console based: I am going to encrypt or decrypt files
+
+	int input;
+	std::cout << "\tWelcome to SECURE FILE ENCRYPTION SYSTEM" << std::endl << std::endl;
+	std::cout << "Here you can secure your data by applying encryption on the files. And Decrypt them to watch the content again." << std::endl;
+	std::cout << "Do your selection:" << std::endl;
+	std::cout << "1- Encryption" << std::endl;
+	std::cout << "2- Decryption" << std::endl;
+	std::cout << std::endl << "Input your choice: ";
+	std::cin >> input;
 	return 0;
 }
